e1d39443fe5c362f94bdd34e27f74f6dd262a3ca
Harsha K P <72432247+harshakp94@users.noreply.github.com>
2024-10-28 14:48:12 +0530

    Update stringCalculator.Tests.cpp

--- stringCalculator.Tests.cpp
+++ stringCalculator.Tests.cpp
@@ -1,1 +1,14 @@
+#include<stringCalculator.h>
+#include<gtest/gtest.h>
+//TEST(TESTSUITE, TestCaseName)
+TEST(string_calculator_add, when_passed_a_single_number_returns_0_for_empty){
+  //Arrange
+  StringCalculator objUndertest;
+  string input = "";
+  int expectedValue = 0;
 
+  //act
+  int actualValue = objUndertest.Add(input);
+  //Assert
+  ASSERT_EQ(expectedValue, actualValue)
+}
