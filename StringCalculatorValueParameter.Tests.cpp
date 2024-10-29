07a8deb38272b189b7d3a7cf9cd84e53dc8772ff
Harsha K P <72432247+harshakp94@users.noreply.github.com>
2024-10-29 10:53:46 +0530

    Update StringCalculatorValueParameter.Tests.cpp

--- StringCalculatorValueParameter.Tests.cpp
+++ StringCalculatorValueParameter.Tests.cpp
@@ -20,7 +20,17 @@ public testing::WithParamInterface<tuple<string,int>>{
 
 };
 INSTANTIATE_TEST_SUITE_P(ValuesDataSet,StringCalculatorAddParameterizedFixture,testing::Values(
-  make_tuple("",0),make_tuple("0",0),make_tuple("1",1),make_tuple("1,2",3)
+  make_tuple("",0),
+  make_tuple("0",0),
+  make_tuple("1",1),
+  make_tuple("1,2",3),
+  make_tuple("1,2,3", 6),
+  make_tuple("1\n2,3",6),
+  make_tuple("//;\n1;2",3),
+  make_tuple("42,1001,3",45),
+  make_tuple("//[***]\n8***2***3",13),
+  make_tuple("//[*][%]\n4*2%3",9),
+  make_tuple("//[**][%^]\n4**1%^9",14)
 ));
 
 
